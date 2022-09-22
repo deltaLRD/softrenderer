27f2d1684756de42541b9e370efc0bfba7bdbc5a
LRD <839122419@qq.com>
2022-09-22 14:31:37 +0800

    environment set done

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#ifndef TINYOBJLOADER_IMPLEMENTATION
+#define TINYOBJLOADER_IMPLEMENTATION
+#endif
+#include "tiny_obj_loader.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Renderer\n";
+    return 0;
+}
